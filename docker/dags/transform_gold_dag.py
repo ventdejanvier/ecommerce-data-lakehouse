14f65239e93d871b604ce765e55dbe5477bb818e
ventdejanvier <22133044@student.hcmute.edu.vn>
2026-06-26 02:06:16 +0700

    Update transform_gold_dag.py

--- docker/dags/transform_gold_dag.py
+++ docker/dags/transform_gold_dag.py
@@ -3,23 +3,22 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=2),
+    "owner": "airflow",
+    "start_date": datetime(2023, 1, 1),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=2),
 }
 
 with DAG(
-    'transform_gold_dag',
+    "transform_gold_dag",
     default_args=default_args,
-    schedule_interval="*/3 * * * *",
+    schedule_interval=None,
     catchup=False,
     max_active_runs=1,
-    tags=['transformation', 'gold']
+    tags=["transformation", "gold"],
 ) as dag:
-
     run_silver_to_gold = BashOperator(
-        task_id='run_silver_to_gold',
-        bash_command='docker exec jupyter-notebook spark-submit --packages io.delta:delta-core_2.12:2.1.0,org.apache.hadoop:hadoop-aws:3.3.2 /home/jovyan/scripts/transform_silver_to_gold.py',
-        pool='spark_heavy',
+        task_id="run_silver_to_gold",
+        bash_command="docker exec jupyter-notebook spark-submit --packages io.delta:delta-core_2.12:2.1.0,org.apache.hadoop:hadoop-aws:3.3.2 /home/jovyan/scripts/transform_silver_to_gold.py",
+        pool="spark_heavy",
     )
